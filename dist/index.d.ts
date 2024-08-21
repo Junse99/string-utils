52626f593a8a00aad2da6eb5efbe627a62bb711c
Sebastian O <sebastian.obregon@pragma.com.co>
2024-08-20 19:34:38 -0500

    feat: add declaration and build

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Capitaliza la primera letra de una cadena.
+ */
+export declare function capitalize(str: string): string;
+/**
+ * Invierte una cadena de texto.
+ */
+export declare function reverse(str: string): string;
+/**
+ * Convierte una cadena en formato kebab-case.
+ */
+export declare function kebabCase(str: string): string;
+/**
+ * Trunca una cadena a una longitud específica.
+ */
+export declare function truncate(str: string, length: number): string;
