aa4669c135d6c107f81817f809bd2a3035c690bb
Sebastian O <sebastian.obregon@pragma.com.co>
2024-08-20 19:26:43 -0500

    feat: add dist

---
+++ dist/__tests__/index.test.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("../index");
+test('capitalize should capitalize the first letter', function () {
+    expect((0, index_1.capitalize)('hello')).toBe('Hello');
+});
+test('reverse should reverse the string', function () {
+    expect((0, index_1.reverse)('hello')).toBe('olleh');
+});
+test('kebabCase should convert string to kebab-case', function () {
+    expect((0, index_1.kebabCase)('Hello World')).toBe('hello-world');
+});
+test('truncate should truncate string to specified length', function () {
+    expect((0, index_1.truncate)('This is a long string', 9)).toBe('This is a...');
+});
